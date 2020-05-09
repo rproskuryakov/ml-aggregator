aa479247e3ca8a8a10f49eb1ccc3637413ba8862
rproskuryakov <rodion43pv@gmail.com>
2020-05-10 03:53:11 +0700

    Integrate FastAPI and Mongo with Motor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,18 @@
+import os
 from typing import List
+from datetime import datetime
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
 
 
 class ExamSchema(BaseModel):
     id: int
     title: str
     description: str
-    created_at: str
+    updated_at: str
     last_updated_by: str
 
 
@@ -18,8 +21,20 @@ class PostExamSchema(BaseModel):
     description: str
 
 
+MONGO_HOST = os.environ.get('MONGO_HOST', 'database')
+MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'secret')
+MONGO_USER = os.environ.get('MONGO_USER', 'mongo')
+
 app = FastAPI()
 
+client = AsyncIOMotorClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}",
+                            username=MONGO_USER,
+                            password=MONGO_PASSWORD,
+                            authSource='mlDatabase',
+                            authMechanism='SCRAM-SHA-256')
+
+app.db = client.mlDatabase
 
 origins = [
     "http://localhost",
@@ -38,13 +53,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 EXAMS = [ExamSchema(**{"created_at": "2018-02-21T15:32:13.188803+00:00",
-                     "description": "Test your knowledge with SQLAlchemy",
-                     "id": 1.0,
-                     "last_updated_by": "script",
-                     "title": "SQLAlchemy Exam",
-                     "updated_at": "2018-02-21T15:32:13.188823+00:00"})
+                       "description": "Test your knowledge with SQLAlchemy",
+                       "id": 1.0,
+                       "last_updated_by": "script",
+                       "title": "SQLAlchemy Exam",
+                       "updated_at": "2018-02-21T15:32:13.188823+00:00"})
     ,
          ExamSchema(
              **{
@@ -61,7 +75,12 @@ EXAMS = [ExamSchema(**{"created_at": "2018-02-21T15:32:13.188803+00:00",
 
 @app.get("/exams")
 async def get_exams() -> List[ExamSchema]:
-    return EXAMS
+    exams = []
+    cursor = app.db.exams.find()
+    for exam in await cursor.to_list(length=10):
+        exam.pop('_id')
+        exams.append(exam)
+    return exams
 
 
 @app.post("/exams")
@@ -70,5 +89,5 @@ async def add_exam(exam: PostExamSchema):
     dictionary.update({"id": len(EXAMS) + 1,
                        "updated_at": "2020-05-09T15:36:04.431417+00:00",
                        "last_updated_by": "HTTP POST Request"})
-    EXAMS.append(ExamSchema(**dictionary))
+    await app.db.exams.insert_one(dictionary)
     return exam
--- init-mongo.js
+++ init-mongo.js
@@ -1,12 +1,32 @@
 db.createUser(
     {
-        user: "admin",
-        pwd: "password",
+        user: "mongo",
+        pwd: "secret",
         roles: [
             {
                 role: "readWrite",
-                db: "my-database"
+                db: "mlDatabase"
             }
         ]
     }
+)
+db.exams.insertMany(
+    [
+        {
+            "created_at": "2018-02-21T15:32:13.188803+00:00",
+            "description": "Test your knowledge with SQLAlchemy",
+            "id": 1.0,
+            "last_updated_by": "script",
+            "title": "SQLAlchemy Exam",
+            "updated_at": "2018-02-21T15:32:13.188823+00:00"
+        },
+        {
+            "created_at": "2018-02-21T15:36:04.431399+00:00",
+            "description": "Tricky questions about Typescript",
+            "id": 2.0,
+            "title": "Typescript Exam",
+            "last_updated_by": "HTTP POST request",
+            "updated_at": "2018-02-21T15:36:04.431417+00:00"
+        }
+    ]
 )
