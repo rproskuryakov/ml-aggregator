cb2f1b357b7ae3c1e6661b34d0596715eb73f997
rproskuryakov <rodion43pv@gmail.com>
2020-05-11 03:54:43 +0700

    Updat yandex crawler

--- web-crawlers/crawler/crawler/spiders/yandex_spider.py
+++ web-crawlers/crawler/crawler/spiders/yandex_spider.py
@@ -3,7 +3,33 @@ import scrapy_splash
 
 
 class YandexSpider(scrapy.Spider):
-    name = 'yandex research'
+    name = 'yandex'
+    lua_script = """
+    function main(splash)
+        splash.private_mode_enabled = false
+        splash:go(splash.args.url)
+        assert(splash:wait(10))
+        local click_button = splash:jsfunc([[
+        function clickButton() {
+            try {
+    	        var button = document.getElementsByClassName('show-more')
+    			            .item(0).getElementsByTagName('button').item(0);
+                button.click();
+                setTimeout(() => { clickButton(); }, 300);
+            }
+            catch (e) {
+                if (e instanceof TypeError) {
+                    console.log('TYPEERROR');
+                    return;
+                }
+            }
+        }
+        ]])
+        click_button()
+        splash:wait(10)
+        return splash:html()
+    end
+    """
 
     def start_requests(self):
         urls = [
@@ -11,7 +37,11 @@ class YandexSpider(scrapy.Spider):
         ]
         for url in urls:
             yield scrapy_splash.SplashRequest(url=url,
-                                              callback=self.parse)
+                                              callback=self.parse,
+                                              endpoint='/execute',
+                                              args={'html': 1,
+                                                    'lua_source': self.lua_script,
+                                                    'wait': 30,})
 
     def parse(self, response):
         for article in response.css('div.publication-item.publication-item_type_horizontal'):
