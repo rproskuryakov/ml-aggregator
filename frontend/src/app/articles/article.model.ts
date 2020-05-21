82b12b1bbcf8bf8e36a80304c1e1859767a15cad
rproskuryakov <rodion43pv@gmail.com>
2020-05-22 00:01:28 +0700

    Update angular article model

--- frontend/src/app/articles/article.model.ts
+++ frontend/src/app/articles/article.model.ts
@@ -1,3 +1,10 @@
+export class ArticleMeta {
+  constructor(
+    public conference?: string,
+    public imageUrl?: string,
+  )
+}
+
 export class Article {
   constructor(
     public name: string,
@@ -6,5 +13,7 @@ export class Article {
     public source: string,
     public researchAreas?: Array<string>,
     public authors?: Array<string>,
+    public meta?: ArticleMeta,
   ) {}
 }
+
